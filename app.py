10b7fa2ae6867af07a98bed2f2318636f73aedc9
drishlekh <111127719+drishlekh@users.noreply.github.com>
2025-11-03 20:16:48 +0530

    Configure app to run on specified port

--- app.py
+++ app.py
@@ -760,7 +760,8 @@ def quiz_complete():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
 
 
 
